4c8833480e7fd346e29bf7639d402564d2512c6c
vitu <vitorcsharp@hotmail.com>
2023-03-19 14:03:17 -0300

    add cometario no main sobre o user e senha definido por min

--- src/main/java/dio/diospringsecurity/DioSpringSecurityApplication.java
+++ src/main/java/dio/diospringsecurity/DioSpringSecurityApplication.java
@@ -11,6 +11,14 @@ public class DioSpringSecurityApplication {
 	//o spring entende que se tive algo ali, ele não precisa ficar gerando o proprio dele
 	//e com isso ele para de gerar a senha temporaria e passa a obedecer o application.properties
 
+	//como vou implementar autorização eu precisei tirar do application.properties o login default que eu defini
+	//que é esse abaixo
+	/*
+	spring.security.user.name=vitu
+	spring.security.user.password=vitu123
+	spring.security.user.roles=USERS
+	* */
+
 	public static void main(String[] args) {
 		SpringApplication.run(DioSpringSecurityApplication.class, args);
 	}
