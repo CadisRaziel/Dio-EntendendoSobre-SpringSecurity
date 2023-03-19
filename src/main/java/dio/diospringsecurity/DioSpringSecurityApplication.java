95cda30a81e56e215d258b8c5452206918fc197f
vitu <vitorcsharp@hotmail.com>
2023-03-19 14:01:57 -0300

    add comentario sobre o application properties

--- src/main/java/dio/diospringsecurity/DioSpringSecurityApplication.java
+++ src/main/java/dio/diospringsecurity/DioSpringSecurityApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DioSpringSecurityApplication {
 
+	//ATENCAO (recurso para demonstração/desenvolvimento apenas)
+	//no application.properties eu coloquei um usuario e uma senha do meu gosto
+	//o spring entende que se tive algo ali, ele não precisa ficar gerando o proprio dele
+	//e com isso ele para de gerar a senha temporaria e passa a obedecer o application.properties
+
 	public static void main(String[] args) {
 		SpringApplication.run(DioSpringSecurityApplication.class, args);
 	}
